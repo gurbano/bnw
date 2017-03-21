a505f27a0f3a69fed498c89e9343036b9b8e9ed9
g.urbano <g.urbano@gmail.com>
2017-03-21 20:22:57 +0100

    secondo commit

--- index.js
+++ index.js
@@ -8,19 +8,46 @@ var BaseEntity = function BaseEntity(tipo, options) {
 	]
 	return this;
 }
-var BEHAVE = function (ID) {return {}}
+var BEHAVE = {
+	DEFAULT : function (ID) {
+		return [];
+	},
+	GET: function (ID,options) {
+		return {};
+	},
+
+}
+
+
+
+
 var Blueprint = {
-	PG: function (options) {
-		var ret = new BaseEntity('PG',options);
-		//ret.behaviours.push(BEHAVE.DEFAULT('PG'))
-		ret.behaviours.push(BEHAVE('CONTROLLED'))
+	E: function (status, base) {
+		var ret = base || new BaseEntity('E',status);
 		return ret;
 	},
-	PNG: function (options) {
-		var ret = new BaseEntity('PNG',options);
-		ret.behaviours.push(BEHAVE('PNG'));
+	PG: function (status, base) {
+		var ret = base || new BaseEntity('PG',status);
+		ret.behaviours.concat(BEHAVE.DEFAULT('PG'))
+		ret.behaviours.push(BEHAVE.GET('CONTROLLED',{}))
 		return ret;
 	},
+	PNG: function (options, base) {
+		var ret = base || new BaseEntity('PNG',status);
+		ret.behaviours.concat(BEHAVE.DEFAULT('PNG'))
+		return ret;
+	},
+	GOBLIN: function (options, base) {
+		var ret = base || new BaseEntity('GOBLIN',status);
+		ret.behaviours.concat(BEHAVE.DEFAULT('GOBLIN'))
+		return ret;
+	},
+	BUILDING: function (options, base) {
+		var ret = base || new BaseEntity('BUILDING',status);
+		ret.behaviours.concat(BEHAVE.DEFAULT('BUILDING'))
+		return ret;
+	},
+
 }
 
 
@@ -30,8 +57,23 @@ var store = {
 		id: 000001,
 		status:{			
 			pos:{x:0, y:0},	
-		},		
-		tipo: 'PG', 
+		},
+		blueprint: 'E|PG', 
+	},{
+		id: 000002,
+		status:{			
+			pos:{x:20, y:10},
+			stats:{
+				FOR: 13,
+				DEX: 9,
+				INT: 4
+			}			
+			behaviours: [
+				{ID:'SCARED', data:{by:001}},
+				{ID:'RUNNING', data:{from:001}},
+			]
+		},
+		blueprint: 'E|PNG|GOBLIN', 
 	}],
 	avatars: []
 
